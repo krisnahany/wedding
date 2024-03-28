2244acb906c2ee2a8b34728772a3c9d50a5019d7
Adiyoga27 <adiyoga27new@gmail.com>
2024-03-28 03:03:00 +0000

    Init

---
+++ index.js
@@ -0,0 +1,11 @@
+const { createServer } = require('http');
+const hostname = '192.168.28.20';
+const port = 4409;
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
